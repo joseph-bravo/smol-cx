efc8c10f80059a26bf08817a775389f6c15e3dbe
joseph-bravo <josephangelobravo@gmail.com>
2022-08-16 07:32:05 -0700

    add favicon and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,34 @@ import LinkGenerator from 'components/LinkGenerator';
 import LinkList from 'components/LinkList';
 import useSWR from 'swr';
 import axios from 'axios';
+import Head from 'next/head';
 
 const Home = () => {
   const { data } = useSWR('/api/links', url =>
     axios(url).then(res => res.data)
   );
   return (
-    <Container className="pb-5">
-      <Row>
-        <Col className="py-5">
-          <h1 className="display-1 text-center mb-5">
-            <i>smol</i>.cx
-          </h1>
-          <i className="bi bi-clipboard"></i>
-          <LinkGenerator />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <LinkList links={data} />
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Head>
+        <title>smol.cx | Link Shortener</title>
+      </Head>
+      <Container className="pb-5">
+        <Row>
+          <Col className="py-5">
+            <h1 className="display-1 text-center mb-5">
+              <i>smol</i>.cx
+            </h1>
+            <i className="bi bi-clipboard"></i>
+            <LinkGenerator />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <LinkList links={data} />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
